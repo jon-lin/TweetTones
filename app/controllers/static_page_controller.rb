024dbb0e41d169818cce7995b1ab07356b82a50e
Jonathan Lin <jclin2013@gmail.com>
2017-06-16 23:46:16 -0400

    Delete all Rails files and unnecessary files and folders

--- app/controllers/static_page_controller.rb
+++
@@ -1,4 +0,0 @@
-class StaticPageController < ApplicationController
-  def root
-  end
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'static_page#root'
-  get 'tweets/index'
-  get 'sentiments/index'
-end
